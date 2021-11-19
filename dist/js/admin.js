45bdcbaf2dccfb10269294559f8b02891f0357aa
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-11-19 20:59:23 +0700

    remove unnecessary function

--- dist/js/admin.js
+++ dist/js/admin.js
@@ -5,10 +5,6 @@ $('.toggle-sidebar').on('click', () => {
     $('.main-container').toggleClass('expand');
 })
 
-// $('.klik').on('click', () => {
-//     $('.dropdown-menu').toggleClass('hide');
-// }, 500);
-
 // Dropdown toggle
 $('ul.nav-menu').on('click', '.dropdown', function() {
     $(this).find('.dropdown-menu').toggleClass('show', 300);
@@ -19,13 +15,3 @@ $('ul.nav-menu').on('click', '.dropdown', function() {
 $('.sidebar.collapse li.dropdown').hover( function() {
     $(this).find('.dropdown-menu').toggleClass('showMenu');
 })
-
-    // $('.nav-list').hover(function() {
-    //     $(this).find('ul').css('opacity', '1');
-    //     $(this).find('ul').css('transform', 'translateY(0%)');
-    //     $(this).find('ul').css('visibility', 'visible');
-    // }, function(e){
-    //     $(this).find('ul').css('opacity', '0');
-    //     $(this).find('ul').css('transform', 'translateY(-25%)');
-    //     $(this).find('ul').css('visibility', 'hidden');
-    // })
