fec2327955fd3748aced598ac73fd794e41041d8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-21 16:33:21 +0700

    pull all style and script from jejak-lulusan-admin

--- dist/js/admin.js
+++ dist/js/admin.js
@@ -24,11 +24,20 @@ $('.toggle-sidebar').on('click', () => {
 // });
 
 // Dropdown toggle
-$('ul.nav-menu').on('click', '.dropdown', function() {
-    $(this).find('.dropdown-menu').toggleClass('show', 300);
-    $(this).find('.dropdown-menu').toggleClass('hide', 300);
-    $(this).find('.dropdown-menu').prev().toggleClass('active-open');
+$('.sidebar.expand ul.nav-menu').on('click', '.dropdown', function() {
+  $(this).find('.dropdown-menu').toggleClass('show', 300);
+  $(this).find('.dropdown-menu').toggleClass('hide', 300);
+  $(this).find('.dropdown-menu').prev().toggleClass('active');
 })
+
+// Prevent dropdown-menu close when click inside
+$('.dropdown-menu').on('click', function(e) {
+e.stopPropagation();
+})
+
+// $('.sidebar.expand ul.nav-menu').on('click', '.dropdown', function(e) {
+//   e.stopPropagation();
+// })
 
 $('.sidebar.collapse li.dropdown').hover( function() {
     $(this).find('.dropdown-menu').toggleClass('showMenu');
